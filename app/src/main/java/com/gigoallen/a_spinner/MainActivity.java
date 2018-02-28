f8c10fb07603fe7e8f00a79df2449591d0b29de0
gigoallen <gigo.allen@gmail.com>
2018-02-28 08:54:08 +0800

    註冊 MainMeal, ViceMeal OnItemsSelectedListener 監聽

--- app/src/main/java/com/gigoallen/a_spinner/MainActivity.java
+++ app/src/main/java/com/gigoallen/a_spinner/MainActivity.java
@@ -2,13 +2,18 @@ package com.gigoallen.a_spinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     String[] viceMeal = {"咖啡", "果汁", "可樂"};
+    static String mainMealOK, mainMeals, viceMealOK, viceMeals;
+    //private Spinner spinnerMainMeal;
+    //private Spinner spinnerViceMeal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
         spinnerMainMeal.setAdapter(adapterMain);
 
+
+
        //取得 程式裡面的 viceMeal 字串陣列
         ArrayAdapter<String> adapterVice =
                 new ArrayAdapter<>(this,
@@ -37,6 +44,53 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_spinner_dropdown_item);
         spinnerViceMeal.setAdapter(adapterVice);
 
+       //註冊 MainMeal OnItemsSelectedListener 監聽
+        spinnerMainMeal.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        mainMealOK = adapterView.getSelectedItem().toString();
+                        mainMeals = "主餐為：" + mainMealOK;
+                    }
 
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                }
+        );
+
+        //註冊 ViceMeal OnItemsSelectedListener 監聽
+        spinnerViceMeal.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        viceMealOK = adapterView.getSelectedItem().toString();
+                        viceMeals = "副餐為：" + viceMealOK;
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                }
+        );
+
+    }
+/*
+    private AdapterView.OnItemClickListener spinnerMainListener =
+            new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                    mainMeal = parent.getSelectedItem().toString();
+                    mainMeals = "主餐為：" + mainMeal;
+                }
+
+            };
+*/
+    public void btnOrder(View view) {
+
+        TextView txtShow = findViewById(R.id.txtShow);
+        txtShow.setText(mainMeals + "\n" + viceMeals);
     }
 }
